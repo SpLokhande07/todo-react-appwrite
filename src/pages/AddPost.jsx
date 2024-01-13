9a7aa71713ec4270546aee6204080978839c3ce6
Saiprasad Lokhande <lokhandesai07@gmail.com>
2024-01-13 19:10:06 +0530

    Working on UI - Home & Post

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, PostForm } from "../components";
+import appwriteService from "../appwrite/config";
 function AddPost() {
+  const [posts, setPosts] = [];
+  useEffect(() => {});
+  appwriteService
+    .getPostList([])
+    .then((posts) => {
+      if (posts) setPosts(posts.documents);
+    })
+    .catch((err) => {});
+
   return (
     <div className="p-y-8">
       <Container>
+        <div className="flex flex-wrap ">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <Postcard post={post} />
+            </div>
+          ))}
+        </div>
         <PostForm />
       </Container>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import appwriteService from "../appwrite/config";
+import { Container, PostCard } from "../components";
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    appwriteService.getPostList().then((posts) => {
+      if (posts) setPosts(posts.documents);
+    });
+  }, []);
+}
+
+export default Home;
